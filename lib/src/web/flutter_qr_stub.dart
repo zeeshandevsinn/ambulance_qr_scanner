21e3215851bf7b5be389cf68bca8d6879b939d6b
ubaidasim7417 <zeeshandevsinn@gmail.com>
2024-12-15 01:50:50 +0500

    Ambulan QR Scanner App

---
+++ lib/src/web/flutter_qr_stub.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:qr_code_scanner_example/qr_code_scanner.dart';
+
+Widget createWebQrView(
+        {onPlatformViewCreated, onPermissionSet, CameraFacing? cameraFacing}) =>
+    const SizedBox();
